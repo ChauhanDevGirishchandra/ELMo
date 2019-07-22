22239c166d5c593aa44f6dfe87843c1da83e0b9c
Dev Chauhan <devchauhanabc3@gmail.com>
2019-07-22 13:18:43 +0530

    another commit

--- classes.py
+++ classes.py
@@ -31,6 +31,7 @@ class biLM(nn.Module):
         self.embedding = embedding
         USE_CUDA = torch.cuda.is_available()
         self.device = torch.device("cuda" if USE_CUDA else "cpu")
+        self.drop = nn.Dropout(p=dropout)
         self.forwardLSTM = nn.LSTM(hidden_size, 
                                          hidden_size, 
                                          n_layers, 
@@ -57,13 +58,13 @@ class biLM(nn.Module):
             initial_states = (torch.zeros(self.n_layers, 1, self.hidden_size, device=self.device), torch.zeros(self.n_layers, 1, self.hidden_size, device=self.device))
         
         for batch_n in range(batch_size):
-            sentence = embedded[:,batch_n, :]
+            b_sentence = embedded[:,batch_n, :]
             length = input_lengths[batch_n]
             
-            
+            sentence = self.drop(b_sentence[:length,:])
             hidden_forward_state, cell_forward_state = initial_states
             hidden_backward_state, cell_backward_state = initial_states
-                
+            
             for t in range(length):
                 output, (hidden_forward_state, cell_forward_state) = self.forwardLSTM(sentence[t].view(1, 1, -1), (hidden_forward_state, cell_forward_state))
                 outputs[t, batch_n, 0, :] = output[0, 0, :]
@@ -93,9 +94,8 @@ class ELMo(nn.Module):
         self.do_layer_norm = do_layer_norm
         self.n_layers = n_layers
         self.biLM = biLM(hidden_size, embedding, n_layers, dropout)
-        self.W = torch.zeros(2*n_layers+1, requires_grad=True, device=self.device)
-        self.W = F.softmax(self.W + 1/(2*n_layers + 1))
-        self.gamma = torch.ones(1, requires_grad=True, device=self.device)
+        self.W = nn.Parameter(torch.tensor([1/(2*n_layers + 1) for i in range(2*n_layers + 1)], requires_grad=True, device=self.device))
+        self.gamma = nn.Parameter(torch.ones(1, requires_grad=True, device=self.device))
         
     def do_norm(layer, mask):
         masked_layer = layer * mask
@@ -109,14 +109,13 @@ class ELMo(nn.Module):
         bilm_outputs, hidden_states, embedded = self.biLM(input_seq, input_lengths, initial_states)
         concat_hidden_with_embedding = torch.cat((embedded.unsqueeze(0), hidden_states), dim=0)
         ELMo_embedding = torch.zeros(*embedded.size(), device=self.device)
-        
         for i in range(2*self.n_layers + 1):
             w = self.W[i]
             layer = concat_hidden_with_embedding[i]
             if self.do_layer_norm:
                 layer = self.do_norm(layer, mask)
             ELMo_embedding = ELMo_embedding + w * layer
-        ELMo_embedding *= self.gamma.item()
+        ELMo_embedding *= self.gamma
         return ELMo_embedding, bilm_outputs
 
 
